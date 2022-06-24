a776302b84510fc48f5a9ec7a192a2f23acf92dd
Daniel Okafor <bucheed@gmail.com>
2022-06-24 07:12:26 +0100

    my first user details commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let nameIdentity = prompt('Whats your name?')
+console.log(nameIdentity);
+let country = prompt('Whats the name of your country?')
+console.log(country);
+let height = prompt('Whats your height?')
+console.log(height);
+
+alert(`
+    User found with the following details:
+    Name: ${nameIdentity}
+    Country: ${country}
+    Tel: ${height}
+`)
